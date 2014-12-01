9ea30571d625642dc97286ee9c642220f50c1c76
CyberSunshine <laurentpower@gmail.com>
2014-12-01 10:39:22 -0500

    split main.js in 4 files

---
+++ js/index.js
@@ -0,0 +1,215 @@
+'use strict'; // Remonte d'avantage les erreurs de code
+
+ //Nombre de page dans chaque chapitre.
+var chapterNb = [[0,'0'],[32,'jpg'],[33,'jpg'],[31,'png'],[34,'png']];
+
+//-------------------On Start----------------//
+$(document).ready(function(){
+
+
+		generateTopList();
+		generateLatestMangaList();
+		
+		//Boutton Random
+		$("#btn_random").click(function(){
+			
+			emptyM1();
+			generateRandomMangaList();
+			$("#main_div_p1_2").show("slow");
+			fakeLoading();
+		});
+
+		//Boutton Latest
+		$("#btn_latest").click(function(){
+			generateLatestMangaList();
+			fakeLoading();
+		});
+		
+		//Boutton A-Z
+		$("#btn_az").click(function(){
+			$container.isotope({ sortBy: 'name' })
+		});
+		
+		//Boutton Collapse
+		$("#btn_collapse").click(function(){
+			
+			var height = parseInt($("#main_div_p1_2").height());
+			//alert(height);
+			if(height>10){
+				$("#main_div_p1_2").hide("slow", emptyM1);
+			}else{
+				generateLatestMangaList();
+				fakeLoading();
+			}
+		});
+		
+	//-------------Isotope-------------//
+		// init Isotope
+		var $container = $('#main_div_p2_2').isotope({
+		itemSelector: '.m2',
+		layoutMode: 'fitRows',
+		getSortData: {
+			name: '.m2_t'
+		}
+	  });
+
+	  // bind sort button click
+		$('#sorts').on( 'click', 'button', function() {
+			var sortValue = $(this).attr('data-sort-value');
+			$container.isotope({ sortBy: sortValue });
+		});
+
+	  // change is-checked class on buttons
+		$('.button-group').each( function( i, buttonGroup ) {
+			var $buttonGroup = $( buttonGroup );
+			$buttonGroup.on( 'click', 'button', function() {
+			$buttonGroup.find('.is-checked').removeClass('is-checked');
+			$( this ).addClass('is-checked');
+			});
+		});
+
+//------------------------Fin Isotope----------------//
+});	//--On start End
+
+
+//Creation d'une liste random
+function generateRandomMangaList(){
+	var div = '';
+	var randomId = '';
+	var id = '';
+	for(var i = 0; i < 12; i++){
+		randomId = getRandomMangaId();
+		div = '<div id="m' + i + '" class="m1">' + 
+		'<div class="m1_img">' +
+		'<a href="manga/somemanga.html"><img class="thumb" src="' + getRandomThumbnail() + '"/></a></div>' +
+		'<p class="m1_t">' + getTrimMangaTitle(getMangaTitleById(randomId),11) + '</p>' +
+		'<p class="m1_d">' + getMangaYearById(randomId) + '</p>' +
+		'<p class="m1_c">Vol: ' + getMangaChapterById(randomId) +'</p>' +
+		'</div>';
+		
+		$( "#main_div_p1_2" ).append( div );
+		id = '#m' + i;
+		$( id ).css( "background", getRandomColor() );
+	}
+}
+//Creation de latest list
+function generateLatestMangaList(){
+	var div = '';
+	var randomId = '';
+	var id = '';
+	
+	emptyM1();
+	
+	for(var i = 0; i < 12; i++){
+		randomId = getRandomMangaId();
+		div = '<div id="m' + i + '" class="m1">' +
+		'<div class="m1_img">' +
+		'<a href="manga/somemanga.html"><img class="thumb" src="' + getRandomThumbnail() + '"/></a></div>' +
+		'<p class="m1_t">' + getTrimMangaTitle(getMangaTitleById(randomId),11) + '</p>' +
+		'<p class="m1_d">' + getRandomReleaseTime() + '</p>' +
+		'<p class="m1_c">Vol: ' + getMangaChapterById(randomId) +'</p>' +
+		'</div>';
+		
+		$( "#main_div_p1_2" ).append( div );
+		id = '#m' + i;
+		$( id ).css( "background", getRandomColor() );
+	}
+	
+	$("#main_div_p1_2").show("slow");
+}
+//Creation d'une liste top
+function generateTopList(){
+	var div = '';
+	var randomId = '';
+	var id = '';
+	for(var i = 0; i <= 50; i++){
+		div = '<div id="m2_' + i + '" class="m2">' + 
+		'<div class="m2_1">' +
+		'<div class="m2_img"></div>' +
+		'<a href="manga/somemanga.html" class="m2_link"><p class="m2_t ">' + getTrimMangaTitle(getMangaTitleById(i),30) + '</p></a>' +
+		'<p class="m2_c">Vol: ' + 	getMangaChapterById(i) + '</p>' +
+		'</div>' +
+		'<div class="m2_2">' +
+		'<p class="m2_d">' + getMangaYearById(i) + '</p>' +
+		'</div>' +
+		'</div>';
+		
+		$( "#main_div_p2_2" ).append( div );
+	}
+}
+
+//getRandomMangaId
+function getRandomMangaId(){
+	var id = 0;
+	id = Math.floor(Math.random() * mangaList.length);
+	return id;
+}
+
+//getMangaTitleById
+function getMangaTitleById(id){
+	var title = mangaList[id][0];
+	return title;
+}
+
+//getTrimMangaTitle
+function getTrimMangaTitle(mTitle, t){
+	var trimTitle = mTitle.length > t ? mTitle.substring(0,t) + '...' : mTitle;
+	return trimTitle;
+}
+
+//getMangaChapterById
+function getMangaChapterById(id){
+	var mChapter = mangaList[id][3];
+	return mChapter;
+}
+
+//getMangaYearById
+function getMangaYearById(id){
+	var mYear = mangaList[id][4];
+	return mYear;
+}
+
+//getRandomReleaseTime
+function getRandomReleaseTime(){
+	var mTime = '';
+	var rnd = Math.floor(Math.random()*5 + 1);
+	
+	if (rnd ==1){
+		mTime = 1 + ' hour ago';
+	}else{
+		mTime = rnd + ' hours ago';
+	}
+	return mTime;
+}
+
+//getRandomReleaseTime
+function getRandomThumbnail(){
+	var rnd = Math.floor(Math.random()*70 + 1);
+	return 'images/small/' + rnd + '.jpg'
+}
+
+//Vider la div p1_2
+function emptyM1(){
+	$("#main_div_p1_2").empty();
+}
+//Fake Loading bar
+function fakeLoading(){
+
+	$( "#main_div_p1_2" ).append( '<div id="progress_bar"></div>' );
+
+	$("#progress_bar").animate({
+      width:'+=50%',
+    }, 300, function() {
+   $( "#progress_bar" ).remove();
+  });
+
+}
+//Creer une couleur hexadecimal au hazard
+function getRandomColor() {
+    var lettres = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += lettres[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
---
+++ js/register.js
@@ -0,0 +1,88 @@
+'use strict'; // Remonte d'avantage les erreurs de code
+
+$(document).ready(function(){
+
+//--------------------------------------------------------------OnStart--------------------------------------register.html
+		
+		//Form submit verification
+		$("form[name='register_form']").submit(register);
+		
+		//Password validation $( "#register-form input[name=password1]" ).keyup(validatePassword);
+		//$( "#id_password1" ).keyup(validatePassword);
+		$("form[name='register_form'] input[name='password1']").keyup(validatePassword);
+		//Password 2 validation
+		$( "form[name='register_form'] input[name=password2]" ).keyup(comparePassword);
+
+});	//--On start End
+
+function register(){
+	
+	var username = $('#id_username').val();
+	
+	if (!validateEmail()){
+		$('#id_email ~ span').html('Invalid email');
+
+		return false;
+	}else if(!validateUsername(username)){
+		var name = $('#id_email').val();
+		if (!validateUsername(name)){
+			$('#id_username ~ span').html('Invalid (3-16 characters)');
+		}
+		return false;
+	}else if(!validatePassword()){
+		return false;
+	}else if(!comparePassword()){
+		return false;
+	}else{
+		return true;
+	}
+}
+function comparePassword(){
+	var pwd1 = $('#id_password1').val();
+	var pwd2 = $('#id_password2').val();
+	
+	if(pwd1!=pwd2){
+		$('#id_password2 ~ span').css('color','red');
+		$('#id_password2 ~ span').html('Passwords not matching');
+		return false;
+	}else{
+		$('#id_password2 ~ span').css('color','green');
+		$('#id_password2 ~ span').html('Ok');	
+		return true;
+	}
+}
+function validateEmail(email) { 
+	var email = $('#id_email').val();
+	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	
+	if (!re.test(email)){
+		$('#id_email ~ span').html('Invalid email');
+		return false;
+	}else{
+		$('#id_email ~ span').html('');
+		return true;
+	}
+} 
+
+//3-16 letters validation
+function validateUsername(user){
+	var re = /^[a-z0-9_-]{3,16}$/
+	return re.test(user);
+}
+
+//6-18 letters validation
+function validatePassword(){	
+	//alert();
+	var password = $('#id_password1').val();
+	var re = /^[a-z0-9_-]{6,18}$/;
+	
+	if (!re.test(password)){
+		$('#id_password1 ~ span').css('color','red');
+		$('#id_password1 ~ span').html('Invalid (6-18 characters)');
+		return false;
+	}else{
+		$('#id_password1 ~ span').css('color','green');
+		$('#id_password1 ~ span').html('Ok');
+		return true;
+	}
+}
